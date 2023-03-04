f8ad4a3aae47e22d56a5decefee4184953a64c7a
Abdallah Ragab <a.ragab8882@gmail.com>
2023-03-04 13:43:36 +0200

    add Base Components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,59 +1,57 @@
 <template>
-<div>
-  <section>FILTER</section>
-  <section>
-    <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as Coach</router-link>
-    </div>
-    <!-- coach data can't be access by vuex because its local (using for loop)
+  <div>
+    <section>FILTER</section>
+    <section>
+      <base-card>
+        <div class="controls">
+          <base-button mode="outline">Refresh</base-button>
+          
+          <!-- link >>>> link =true -->
+          <base-button link to="/register">Register as Coach</base-button>
+        </div>
+        <!-- coach data can't be access by vuex because its local (using for loop)
     so u should use props to pass it from coachList to CoachItem -->
-    <ul v-if="hasCoaches">
-      <coach-item
-        v-for="coach in filteredCoaches"
-        :key="coach.id"
-        :id="coach.id"
-        :first-name="coach.firstName"
-        :last-name="coach.lastName"
-        :rate="coach.hourlyRate"
-        :areas="coach.areas"
-      ></coach-item>
-    </ul>
-    <h3 v-else>No coaches found.</h3>
-  </section>
+        <ul v-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :first-name="coach.firstName"
+            :last-name="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No coaches found.</h3>
+      </base-card>
+    </section>
   </div>
 </template>
 
 <script>
-import CoachItem from '../../components/coaches/CoachItem.vue';
+import CoachItem from "../../components/coaches/CoachItem.vue";
+
 export default {
   components: {
     CoachItem,
   },
   computed: {
-    
     filteredCoaches() {
-      return this.$store.getters['coaches/coaches'];
+      return this.$store.getters["coaches/coaches"];
     },
     hasCoaches() {
-      return this.$store.getters['coaches/hasCoaches'];// access through named space(coaches)
-    }
-  }
-}
+      // access through named space(coaches)
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
 </script>
 
 <style scoped>
-
 ul {
-  
-  display: flex;
-  flex-direction: column;
-  /* justify-content: center; */
-  /* justify-items: center; */
   list-style: none;
-  margin: 1rem auto ;
+  margin: 0;
   padding: 0;
-  width: 30rem;
 }
 
 .controls {
